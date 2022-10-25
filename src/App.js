8c1fed23471c896d8f909a445418df2dd4e82a15
Lucas Calabria <lucascalabria86@gmail.com>
2022-10-25 11:45:12 -0300

    Renderrização Condicional React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import styled, { createGlobalStyle } from "styled-components";
 import TelaLogin from "./components/TelaLogin/TelaLogin";
 import TelaCadastro from "./components/TelaCadastro/TelaCadastro";
+import {useState} from 'react'
+import TelaUsuarioCadastrado from './components/TelaUsuarioCadastrado/TelaUsuarioCadastrado'
 
 const GlobalStyled = createGlobalStyle`
   *{
@@ -14,12 +16,34 @@ const MainContainer = styled.main`
 `
 
 function App() {
+  const [tela, setTela] = useState(1)
+
+  function renderizaTela() {
+    if(tela ===1) {
+      return <TelaLogin mudaTela={mudaTela} />
+    } else if (tela ===2 ) {
+      return <TelaCadastro mudaTela={mudaTela} telaFinal={telaFinal} />
+    } else {
+      return <TelaUsuarioCadastrado />
+    }
+  }
+
+  const telaFinal = () => {
+    setTela(3)
+  }
+
+  const mudaTela = () => {
+    if(tela ===1) {
+      setTela(2)
+    }else if(tela ===2) {
+      setTela(1)
+    }
+  }
 
   return (
     <MainContainer >
       <GlobalStyled />
-      <TelaLogin />
-      <TelaCadastro />
+      {renderizaTela()}
     </MainContainer>
   );
 }
